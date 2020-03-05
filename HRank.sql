b763cd04881d43067bf3935e3895daa2334c0dd2
OldProject1 <47574197+OldProject1@users.noreply.github.com>
2020-03-05 10:02:32 +0300

    Update HRank.sql

--- HRank.sql
+++ HRank.sql
@@ -1,2 +1,2 @@
-SELECT CONCAT(Name , '(', LEFT(Occupation,1), ')')
-FROM OCCUPATIONS;
+SELECT CONCAT(Name, '(', LEFT(Occupation, 1), ')') FROM OCCUPATIONS ORDER BY Name;
+SELECT CONCAT('There are a total of ', COUNT(*), ' ',Lower(Occupation), 's') FROM OCCUPATIONS ORDER BY COUNT(*), Occupation;
